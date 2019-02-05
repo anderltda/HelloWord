3b88deb5a508ad4f9fd5793bee29d0f7492416b4
Usuário Convidado <Guest@pa06micro03.local>
2019-02-04 23:14:05 -0200

    teste

--- HelloWord/AppDelegate.swift
+++ HelloWord/AppDelegate.swift
@@ -16,29 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("App acabou de ser launcheado!! (application) ")
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("Acabou de sair do estado ativo (applicationWillResignActive)")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("App está em background (applicationDidEnterBackground)")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("App prestes a se tornar ativo (applicationWillEnterForeground)")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("Aplicativo está ativo e pronto para ser utilizado (applicationDidBecomeActive)")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("Usuario encerrou o app (applicationWillTerminate)")
     }
 
 
--- HelloWord/SecondViewController.swift
+++ HelloWord/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  HelloWord
 //
 //  Created by Usuário Convidado on 04/02/19.
@@ -8,32 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Tela 1: (viewDidLoad)")
+        print("Tela 2: (viewDidLoad)")
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("Tela 1: (viewWillAppear)")
+        print("Tela 2: (viewWillAppear)")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("Tela 1: (viewDidAppear)")
+        print("Tela 2: (viewDidAppear)")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("Tela 1: (viewWillDisappear)")
+        print("Tela 2: (viewWillDisappear)")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("Tela 1: (viewDidDisappear)")
+        print("Tela 2: (viewDidDisappear)")
     }
 
 }
-
